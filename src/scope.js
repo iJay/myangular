d9a4adae4fa3fca30232add40ab12a806720405f
hanbing1991 <hblx2017@163.com>
2024-06-13 10:25:02 +0800

    1-6-keeping-the-digest-going-while-it-stays-dirty

--- src/scope.js
+++ src/scope.js
@@ -9,24 +9,35 @@ function initWatchValue () {}
 Scope.prototype.$watch = function (watchFn, listenerFn) {
   var watcher = {
     watchFn,
-    listenerFn,
+    listenerFn: listenerFn || function () {},
     last: initWatchValue
   }
   this.$$watchers.push(watcher);
 }
 
-Scope.prototype.$digest = function () {
+// 这个函数能把所有 watcher 都执行一次，最终返回一个标识本轮是否发生了变化的布尔值
+Scope.prototype.$digestOnce = function () {
   var self = this;
-  var newValue, oldValue
+  var newValue, oldValue, dirty;
   _.forEach(this.$$watchers, function(watcher) {
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
     if (newValue !== oldValue) {
       watcher.last = newValue;
       watcher.listenerFn(newValue, (oldValue === initWatchValue ? newValue : oldValue), self);
+      dirty = true;
     }
-    
   })
+  return dirty;
+}
+
+// $digest 至少会对所有 watcher 进行一轮遍历
+// 只要发现值发生了变化就一直调用 $$digestOnce
+Scope.prototype.$digest = function () {
+  var dirty;
+  do {
+    dirty = this.$digestOnce()
+  } while (dirty)
 }
 
 module.exports = Scope;
